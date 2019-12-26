1566723c8d3c07e664006a8559da5eabd138bf61
Dan <55209299+eco-d@users.noreply.github.com>
2019-12-26 14:23:31 -0500

    diy_server_client

---
+++ app.js
@@ -0,0 +1,57 @@
+// replace these values with those generated in your TokBox Account
+var apiKey = "YOUR_API_KEY";
+var sessionId = "YOUR_SESSION_ID";
+var token = "YOUR_TOKEN";
+
+// (optional) add server code here
+    var SERVER_BASE_URL = 'http://localhost:8080';
+    fetch(SERVER_BASE_URL + '/session').then(function(res) {
+      return res.json();
+    }).then(function(res) {
+      apiKey = res.apiKey;
+      sessionId = res.sessionId;
+      token = res.token;
+      initializeSession();
+    }).catch(handleError);
+
+
+
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
+
+function initializeSession() {
+  var session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+
+  // Create a publisher
+  var publisher = OT.initPublisher('publisher', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      session.publish(publisher, handleError);
+    }
+  });
+}
+
+session.on('streamCreated', function(event) {
+  session.subscribe(event.stream, 'subscriber', {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%'
+  }, handleError);
+});
+
+initializeSession();
